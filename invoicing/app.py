71ac76f2cec90ea71bfdb94a93a6195523cbe14a
Michael Hagarty <michael@spriggy.com.au>
2019-02-16 18:55:47 +1100

    Don't parse numbers in JSON.

--- invoicing/app.py
+++ invoicing/app.py
@@ -2,10 +2,19 @@
 from __future__ import absolute_import
 
 from flask import Flask, jsonify
+from flask.json import JSONDecoder
+
+
+class CustomJSONDecoder(JSONDecoder):
+    def __init__(self, *args, **kwargs):
+        kwargs['parse_float'] = str
+
+        super(CustomJSONDecoder, self).__init__(*args, **kwargs)
 
 
 def create_app():
     app = Flask('invoicing')
+    app.json_decoder = CustomJSONDecoder
 
     @app.route('/')
     def index():
